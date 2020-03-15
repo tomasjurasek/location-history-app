0e1f231de8541748dd39c6bbcbbf29f9b62c27fe
Lukáš Marek <lukas_marek@kb.cz>
2020-03-15 20:56:16 +0100

    ✨ Load locations from API

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -14,11 +14,14 @@
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+
 
 @Component({})
 export default class Home extends Vue {
