50efaf758b89315609976899a3750ee3d66cb4ed
Lukáš Marek <lukas_marek@kb.cz>
2020-03-15 13:57:10 +0100

    ✨ Instructions how to use Google Takeout

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
+import Instructions from "@/views/Instructions.vue";
 
 Vue.use(VueRouter);
 
@@ -15,7 +16,13 @@ const routes = [
         path: "/about",
         name: "About",
         component: About
+    },
+    {
+        path: "/instructions",
+        name: "Instructions",
+        component: Instructions
     }
+
 ];
 
 const router = new VueRouter({
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="pa-3">
-        <p>Historii polohy stáhněte z Google podle <a href="/navod">návodu</a>, výsledný soubor je nazvaný např. <em>takeout-20200315T062605Z-001.zip</em>, ten nahrajte zde.</p>
+        <p>Historii polohy stáhněte z Google podle <v-btn text :to="{ name: 'Instructions' }" color="primary">návodu</v-btn>, výsledný soubor je nazvaný např. <em>takeout-20200315T062605Z-001.zip</em>, ten nahrajte zde.</p>
         <v-form>
             <v-file-input accept=".zip" label="Soubor"></v-file-input>
-            <v-btn>Nahrát</v-btn>
+            <v-btn class="ma-2">Nahrát</v-btn>
+            <v-btn class="ma-2" :to="{ name: 'Instructions' }">Jak připravit data?</v-btn>
         </v-form>
     </div>
 </template>
