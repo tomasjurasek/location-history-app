d2b3b5bf2d4a5ffbdb64da743318380f45675d37
Jan Zak <zj@zakjan.cz>
2020-03-16 15:20:32 +0100

    Increase spaces to 4

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,46 +1,47 @@
 <template>
-  <div class="pa-3">
-    <v-alert type="warning" v-if="!$route.params.id">
-      <strong>Chybí ID uživatele!</strong> <br />
-      Prosím, použijte odkaz, který vám přišel do mailu.
-    </v-alert>
+    <div class="pa-3">
+        <v-alert type="warning" v-if="!$route.params.id">
+            <strong>Chybí ID uživatele!</strong> <br />
+            Prosím, použijte odkaz, který vám přišel do mailu.
+        </v-alert>
 
-    <v-alert type="warning" v-if="uploadFailed">
-      <strong>Něco se pokazilo!</strong> <br />
-      Prosím, zkuste to znovu.
-    </v-alert>
+        <v-alert type="warning" v-if="uploadFailed">
+            <strong>Něco se pokazilo!</strong> <br />
+            Prosím, zkuste to znovu.
+        </v-alert>
 
-    <p>
-      Historii polohy stáhněte z Google podle
-      <v-btn text :to="{ name: 'Instructions' }" color="primary">návodu</v-btn>,
-      výsledný soubor je nazvaný např.
-      <em>takeout-20200315T062605Z-001.zip</em>, ten nahrajte zde.
-    </p>
-    <v-form>
-      <v-file-input
-        show-size
-        counter
-        label="Soubor"
-        v-model="file"
-      ></v-file-input>
-      <v-btn
-        class="primary ma-2"
-        v-on:click="submitFile()"
-        :disabled="!file"
-        :loading="loading"
-        >Nahrát</v-btn
-      >
-      <v-btn class="ma-2" :to="{ name: 'Instructions' }"
-        >Jak připravit data?</v-btn
-      >
-    </v-form>
+        <p>
+            Historii polohy stáhněte z Google podle
+            <v-btn text :to="{ name: 'Instructions' }" color="primary"
+                >návodu</v-btn
+            >, výsledný soubor je nazvaný např.
+            <em>takeout-20200315T062605Z-001.zip</em>, ten nahrajte zde.
+        </p>
+        <v-form>
+            <v-file-input
+                show-size
+                counter
+                label="Soubor"
+                v-model="file"
+            ></v-file-input>
+            <v-btn
+                class="primary ma-2"
+                v-on:click="submitFile()"
+                :disabled="!file"
+                :loading="loading"
+                >Nahrát</v-btn
+            >
+            <v-btn class="ma-2" :to="{ name: 'Instructions' }"
+                >Jak připravit data?</v-btn
+            >
+        </v-form>
 
-    <div class="mt-8">
-      <v-btn :to="{ name: 'Map' }">
-        Mapa
-      </v-btn>
+        <div class="mt-8">
+            <v-btn :to="{ name: 'Map' }">
+                Mapa
+            </v-btn>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped></style>
@@ -52,34 +53,34 @@ import axios from "axios";
 
 @Component({})
 export default class Home extends Vue {
-  file!: Blob;
-  uploadFailed = false;
-  loading = false;
+    file!: Blob;
+    uploadFailed = false;
+    loading = false;
 
-  submitFile() {
-    this.uploadFailed = false;
-    this.loading = true;
-    const formData = new FormData();
-    formData.append("file", this.file);
-    axios
-      .post(
-        `${process.env.VUE_APP_API_URL}/users/${this.$route.params.id}/file`,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data"
-          }
-        }
-      )
-      .then(() => {
-        this.loading = false;
-        console.log("Hotovo");
-      })
-      .catch(e => {
-        this.loading = false;
-        console.log("Chybka", e);
-        this.uploadFailed = true;
-      });
-  }
+    submitFile() {
+        this.uploadFailed = false;
+        this.loading = true;
+        const formData = new FormData();
+        formData.append("file", this.file);
+        axios
+            .post(
+                `${process.env.VUE_APP_API_URL}/users/${this.$route.params.id}/file`,
+                formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    }
+                }
+            )
+            .then(() => {
+                this.loading = false;
+                console.log("Hotovo");
+            })
+            .catch(e => {
+                this.loading = false;
+                console.log("Chybka", e);
+                this.uploadFailed = true;
+            });
+    }
 }
 </script>
--- client/src/views/Instructions.vue
+++ client/src/views/Instructions.vue
@@ -1,69 +1,70 @@
 <template>
-  <div class="pa-3">
-    <h1>Jak na to?</h1>
+    <div class="pa-3">
+        <h1>Jak na to?</h1>
 
-    <h2>Přihlaste se do Google Takeout</h2>
-    <p>
-      <a
-        href="https://takeout.google.com/u/0/settings/takeout?pli=1"
-        target="_blank"
-        >https://takeout.google.com/u/0/settings/takeout?pli=1</a
-      ><br />
-      Klikněte na <strong>Zrušit výběr všech</strong> a vyberte pouze historii
-      polohy.
-    </p>
-    <v-sheet class="pa-12" color="white">
-      <v-img
-        src="instructions/instructions1.png"
-        max-width="500"
-        class="elevation-4"
-      />
-    </v-sheet>
-    <p>
-      Nechte formát dat JSON a na konci stránky klepněte na
-      <strong>pokračovat</strong>.
-    </p>
-    <v-sheet class="pa-12" color="white">
-      <v-img
-        src="instructions/instructions2.png"
-        max-width="500"
-        class="elevation-4"
-      />
-      <br />
-    </v-sheet>
-    <p>
-      Nechte si odkaz zaslat e-mailem, zvolte exportovat jednou a klikněte na
-      <strong>Vytvořit export</strong>. Objeví se následující obrazovka s
-      informací, že dochází ke stahování.
-    </p>
-    <v-sheet class="pa-12" color="white">
-      <v-img
-        src="instructions/instructions3.png"
-        max-width="500"
-        class="elevation-4"
-      />
-    </v-sheet>
-    <h2>Čekejte</h2>
-    <p>Po několika minutách se objeví následující obrazovka:</p>
-    <v-sheet class="pa-12" color="white">
-      <v-img
-        src="instructions/instructions4.png"
-        max-width="500"
-        class="elevation-4"
-      />
-    </v-sheet>
-    <p>
-      Klepněte na <strong>Stáhnout</strong> a pro ověření se znovu přihlaste
-      svým Google účtem. Následně se Vám na disk stáhne soubor s názvem
-      (například) <code>takeout-20200314T194428Z-001.zip</code>
-    </p>
-    <h2>Nahrajte soubor</h2>
-    <p>
-      Tento soubor nahrajte na
-      <v-btn class="ma-2" :to="{ name: 'Home' }">úvodní stránce</v-btn>.
-      Děkujeme!
-    </p>
-  </div>
+        <h2>Přihlaste se do Google Takeout</h2>
+        <p>
+            <a
+                href="https://takeout.google.com/u/0/settings/takeout?pli=1"
+                target="_blank"
+                >https://takeout.google.com/u/0/settings/takeout?pli=1</a
+            ><br />
+            Klikněte na <strong>Zrušit výběr všech</strong> a vyberte pouze
+            historii polohy.
+        </p>
+        <v-sheet class="pa-12" color="white">
+            <v-img
+                src="instructions/instructions1.png"
+                max-width="500"
+                class="elevation-4"
+            />
+        </v-sheet>
+        <p>
+            Nechte formát dat JSON a na konci stránky klepněte na
+            <strong>pokračovat</strong>.
+        </p>
+        <v-sheet class="pa-12" color="white">
+            <v-img
+                src="instructions/instructions2.png"
+                max-width="500"
+                class="elevation-4"
+            />
+            <br />
+        </v-sheet>
+        <p>
+            Nechte si odkaz zaslat e-mailem, zvolte exportovat jednou a klikněte
+            na
+            <strong>Vytvořit export</strong>. Objeví se následující obrazovka s
+            informací, že dochází ke stahování.
+        </p>
+        <v-sheet class="pa-12" color="white">
+            <v-img
+                src="instructions/instructions3.png"
+                max-width="500"
+                class="elevation-4"
+            />
+        </v-sheet>
+        <h2>Čekejte</h2>
+        <p>Po několika minutách se objeví následující obrazovka:</p>
+        <v-sheet class="pa-12" color="white">
+            <v-img
+                src="instructions/instructions4.png"
+                max-width="500"
+                class="elevation-4"
+            />
+        </v-sheet>
+        <p>
+            Klepněte na <strong>Stáhnout</strong> a pro ověření se znovu
+            přihlaste svým Google účtem. Následně se Vám na disk stáhne soubor s
+            názvem (například) <code>takeout-20200314T194428Z-001.zip</code>
+        </p>
+        <h2>Nahrajte soubor</h2>
+        <p>
+            Tento soubor nahrajte na
+            <v-btn class="ma-2" :to="{ name: 'Home' }">úvodní stránce</v-btn>.
+            Děkujeme!
+        </p>
+    </div>
 </template>
 
 <style scoped></style>
