2557b6461ddd726c183db0f2659825d76f3048aa
Lubos Hladik <lubos@hladikovi.net>
2020-03-16 20:29:20 +0100

    Added getting extracted JSON file explicitly for EN and CZ, try to get first JSON file for any other.

--- server/API/API/Controllers/UsersController.cs
+++ server/API/API/Controllers/UsersController.cs
@@ -44,9 +44,7 @@ namespace API.Controllers
                 var extractedDirectoryPath = Directory.CreateDirectory(Path.Combine(tempDirectoryPath, "data"));
                 ZipFile.ExtractToDirectory(uploadedFilePath, extractedDirectoryPath.FullName);
 
-                var jsonPath = Path.Combine(extractedDirectoryPath.FullName, "Takeout", "Location History",
-                    "Location History.json");
-                var jsonData = await System.IO.File.ReadAllTextAsync(jsonPath);
+                var jsonData = await GetJsonData(extractedDirectoryPath.FullName);
                 var locations = await locationService.CreateUserLocationsAsync(userId, jsonData);
                 response.Locations = locations.Select(s => new LocationViewModel
                 {
@@ -76,6 +74,60 @@ namespace API.Controllers
             }
 
             return response;
+        }
+
+        private static async Task<string> GetJsonData(string directoryPath)
+        {
+            var jsonPathEn = Path.Combine(directoryPath, "Takeout", "Location History", "Location History.json");
+            var jsonPathCz = Path.Combine(directoryPath, "Takeout", "Historie polohy", "Historie polohy.json");
+            string finalJsonPath = null;
+
+            if (System.IO.File.Exists(jsonPathEn))
+            {
+                finalJsonPath = jsonPathEn;
+            }
+            else if (System.IO.File.Exists(jsonPathCz))
+            {
+                finalJsonPath = jsonPathCz;
+            }
+            else
+            {
+                if (!TryGetSingleJsonFile(directoryPath, out finalJsonPath))
+                {
+                    throw new Exception($"JSON file with location history not found in '{directoryPath}'.");
+                }
+            }
+
+            var jsonData = await System.IO.File.ReadAllTextAsync(finalJsonPath);
+            return jsonData;
+        }
+
+        private static bool TryGetSingleJsonFile(string directoryPath, out string jsonFilePath)
+        {
+            var dir = new DirectoryInfo(directoryPath);
+            var files = dir.EnumerateFiles("*.json", SearchOption.TopDirectoryOnly).ToList();
+            if (files.Count == 1)
+            {
+                jsonFilePath = files.Single().FullName;
+                return true;
+            }
+
+            if (files.Count > 1)
+            {
+                jsonFilePath = default;
+                return false;
+            }
+
+            foreach (var subdir in dir.EnumerateDirectories())
+            {
+                if (TryGetSingleJsonFile(subdir.FullName, out jsonFilePath))
+                {
+                    return true;
+                }
+            }
+
+            jsonFilePath = default;
+            return false;
         }
     }
 }
