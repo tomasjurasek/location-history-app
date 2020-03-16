84bba80e42a3524dabc168c4a4b72dfbe492b2f3
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-03-16 15:56:02 +0100

    add introduction to Home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,39 +1,49 @@
 <template>
   <div class="pa-3">
-    <v-alert type="warning" v-if="!$route.params.id">
-      <strong>Chybí ID uživatele!</strong> <br />
-      Prosím, použijte odkaz, který vám přišel do mailu.
-    </v-alert>
-
-    <v-alert type="warning" v-if="uploadFailed">
-      <strong>Něco se pokazilo!</strong> <br />
-      Prosím, zkuste to znovu.
-    </v-alert>
-
-    <p>
-      Historii polohy stáhněte z Google podle
-      <v-btn text :to="{ name: 'Instructions' }" color="primary">návodu</v-btn>,
-      výsledný soubor je nazvaný např.
-      <em>takeout-20200315T062605Z-001.zip</em>, ten nahrajte zde.
-    </p>
-    <v-form>
-      <v-file-input
-        show-size
-        counter
-        label="Soubor"
-        v-model="file"
-      ></v-file-input>
-      <v-btn
-        class="primary ma-2"
-        v-on:click="submitFile()"
-        :disabled="!file"
-        :loading="loading"
-        >Nahrát</v-btn
-      >
-      <v-btn class="ma-2" :to="{ name: 'Instructions' }"
-        >Jak připravit data?</v-btn
-      >
-    </v-form>
+    <v-container fill-height fluid>
+      <v-row align="center" justify="center">
+        <v-alert type="warning" v-if="!$route.params.id">
+          <strong>Chybí ID uživatele!</strong> <br />
+          Prosím, použijte odkaz, který vám přišel do mailu.
+        </v-alert>
+        <v-alert type="warning" v-if="uploadFailed">
+          <strong>Něco se pokazilo!</strong> <br />
+          Prosím, zkuste to znovu.
+        </v-alert>
+      </v-row>
+      <v-row align="center" justify="center">
+        <p>
+          Historii polohy stáhněte z Google podle
+          <router-link :to="{ name: 'Instructions' }" color="primary"
+            >návodu</router-link
+          >, výsledný soubor je nazvaný např.
+          <em>takeout-20200315T062605Z-001.zip</em>, ten nahrajte zde.
+        </p>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-form>
+          <v-file-input
+            show-size
+            counter
+            label="Soubor"
+            v-model="file"
+          ></v-file-input>
+          <v-btn
+            class="primary ma-2"
+            v-on:click="submitFile()"
+            :disabled="!file"
+            :loading="loading"
+            >Nahrát</v-btn
+          >
+          <!-- <v-btn class="ma-2" :to="{ name: 'Instructions' }"
+                >Jak připravit data?</v-btn
+              > -->
+        </v-form>
+      </v-row>
+      <v-row align="center" justify="center">
+        <Instructions />
+      </v-row>
+    </v-container>
 
     <div class="mt-8">
       <v-btn :to="{ name: 'Map' }">
@@ -49,13 +59,13 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import axios from "axios";
+import Instructions from "./Instructions.vue";
 
-@Component({})
+@Component({ components: { Instructions } })
 export default class Home extends Vue {
   file: any = null;
   uploadFailed = false;
   loading = false;
-
   submitFile() {
     this.uploadFailed = false;
     this.loading = true;
