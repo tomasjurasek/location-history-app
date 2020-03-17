871e928266ef17d11469bc3b707f9c3eb152bcd7
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2020-03-17 18:33:55 +0100

    update

--- server/API/API/Controllers/UsersController.cs
+++ server/API/API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Models;
+using API.ServiceBus;
 using API.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -18,27 +19,32 @@ namespace API.Controllers
     {
         private readonly UserLocationsService locationService;
         private readonly ILogger<UsersController> logger;
-        private readonly IWebHostEnvironment env;
         private readonly IConfiguration config;
+        private readonly AzureBlobService azureBlobService;
+        private readonly LocationCreatedSender locationCreatedSender;
 
-        public UsersController(UserLocationsService locationService, ILogger<UsersController> logger, IWebHostEnvironment env, IConfiguration config)
+        public UsersController(UserLocationsService locationService,
+            ILogger<UsersController> logger,
+            IConfiguration config,
+            AzureBlobService azureBlobService,
+            LocationCreatedSender locationCreatedSender
+            )
         {
             this.locationService = locationService;
             this.logger = logger;
-            this.env = env;
             this.config = config;
+            this.azureBlobService = azureBlobService;
+            this.locationCreatedSender = locationCreatedSender;
         }
 
         [HttpPost("{userId}/file")]
         [RequestSizeLimit(104857600)]
-        //[RequestFormLimits(MultipartBodyLengthLimit = 104857600)]
         public async Task<ActionResult<UserLocationViewModel>> UploadFileAsync(string userId, [FromForm] IFormFile file)
         {
             var response = new UserLocationViewModel
             {
                 Id = userId
             };
-            string tempDirectoryPath = null;
 
             if (file == null)
             {
@@ -52,47 +58,23 @@ namespace API.Controllers
 
             try
             {
-                //var userFolderPath = $"{Directory.GetCurrentDirectory()}/wwwroot/{userId}";
-                tempDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", $"{userId}");
-                Directory.CreateDirectory(tempDirectoryPath);
-
-                var uploadedFilePath = Path.Combine(tempDirectoryPath, file.FileName);
-                await using (Stream stream = new FileStream(uploadedFilePath, FileMode.Create))
+                using (Stream stream = new MemoryStream())
                 {
                     await file.CopyToAsync(stream);
+                    stream.Position = 0;
+                    await azureBlobService.UploadFile(userId, stream);
+                    await locationCreatedSender.SendMessageAsync(new LocationsCreatedMessage
+                    {
+                        UserId = userId
+                    });
                 }
 
-                //var extractedDirectoryPath = Directory.CreateDirectory(Path.Combine(tempDirectoryPath, "data"));
-                //ZipFile.ExtractToDirectory(uploadedFilePath, extractedDirectoryPath.FullName);
-
-                var jsonFilePath = GetJsonFilePath(extractedDirectoryPath.FullName);
-                var locations = await locationService.CreateUserLocationsAsync(userId, jsonFilePath);
-                response.Locations = locations.Select(s => new LocationViewModel
-                {
-                    DateTimeUtc = s.DateTimeUtc,
-                    Accuracy = s.Accuracy,
-                    Latitude = s.Latitude,
-                    Longitude = s.Longitude
-                }).ToList();
             }
             catch (Exception ex)
             {
                 throw new Exception("Processing of uploaded file failed.", ex);
             }
-            finally
-            {
-                try
-                {
-                    if (tempDirectoryPath != null)
-                    {
-                        Directory.Delete(tempDirectoryPath, true);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "Deleting of temporary folder '{TempDirectoryPath}' failed. Exception: {Exception}.", tempDirectoryPath, ex.ToString());
-                }
-            }
+            
 
             return response;
         }
@@ -107,62 +89,6 @@ namespace API.Controllers
 
 
             return file.Length <= maxMaxUploadedFileLength;
-        }
-
-        private bool IsFileContentTypeValid(IFormFile file)
-        {
-            return file.ContentType == "application/zip";
-        }
-
-        private string GetJsonFilePath(string directoryPath)
-        {
-            var jsonPathEn = Path.Combine(directoryPath, "Takeout", "Location History", "Location History.json");
-            var jsonPathCz = Path.Combine(directoryPath, "Takeout", "Historie polohy", "Historie polohy.json");
-
-            if (System.IO.File.Exists(jsonPathEn))
-            {
-                return jsonPathEn;
-            }
-
-            if (System.IO.File.Exists(jsonPathCz))
-            {
-                return jsonPathCz;
-            }
-
-            if (!TryGetSingleJsonFile(directoryPath, out var finalJsonPath))
-            {
-                throw new Exception($"JSON file with location history not found in '{directoryPath}'.");
-            }
-
-            return finalJsonPath;
-        }
-
-        private bool TryGetSingleJsonFile(string directoryPath, out string jsonFilePath)
-        {
-            var dir = new DirectoryInfo(directoryPath);
-            var files = dir.EnumerateFiles("*.json", SearchOption.TopDirectoryOnly).ToList();
-            if (files.Count == 1)
-            {
-                jsonFilePath = files.Single().FullName;
-                return true;
-            }
-
-            if (files.Count > 1)
-            {
-                jsonFilePath = default;
-                return false;
-            }
-
-            foreach (var subdir in dir.EnumerateDirectories())
-            {
-                if (TryGetSingleJsonFile(subdir.FullName, out jsonFilePath))
-                {
-                    return true;
-                }
-            }
-
-            jsonFilePath = default;
-            return false;
         }
     }
 }
