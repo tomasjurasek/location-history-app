fc2bd3b849b7e948be3ce887c6549b80789f5bad
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2020-03-17 18:13:12 +0100

    Add queue receiver

--- server/API/API/Services/FileParseBackgroundService.cs
+++ server/API/API/Services/FileParseBackgroundService.cs
@@ -1,82 +1,106 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using API.ServiceBus;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace API.Services
 {
-    public class FileParseBackgroundService : BackgroundService
+    public abstract class ServiceBusReceiver
+    {
+        protected readonly IQueueClient _queueClient;
+        private readonly ILogger<ServiceBusReceiver> logger;
+
+        public ServiceBusReceiver(IConfiguration configuration, ILogger<ServiceBusReceiver> logger)
+        {
+            var cs = configuration.GetValue<string>("AzureServiceBus");
+            _queueClient = new QueueClient(cs, "locationfilequeue");
+            this.logger = logger;
+        }
+
+        public virtual void RegisterMessageHandler()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 10,
+                AutoComplete = false
+            };
+            _queueClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+        }
+
+        protected virtual async Task ProcessMessageAsync(Message message, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var messageBody = Encoding.UTF8.GetString(message.Body);
+                var eventData = JsonConvert.DeserializeObject<LocationsCreatedMessage>(messageBody);
+
+                await ProcessEventAsync(eventData, cancellationToken);
+                await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            }
+            catch (Exception ex)
+            {
+                var messageBody = Encoding.UTF8.GetString(message.Body);
+                logger.LogError(ex, $"Could not process message. Message body: {messageBody}");
+                throw;
+            }
+        }
+
+        protected abstract Task ProcessEventAsync(LocationsCreatedMessage message, CancellationToken cancellationToken);
+
+        protected virtual Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            logger.LogError(exceptionReceivedEventArgs.Exception, "Queue receiver error");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class LocationCreatedReceiver : ServiceBusReceiver
     {
         private readonly UserLocationsService userLocationsService;
-        private readonly ILogger<FileParseBackgroundService> logger;
         private readonly AzureBlobService azureBlobService;
 
-        public FileParseBackgroundService(UserLocationsService userLocationsService, ILogger<FileParseBackgroundService> logger, AzureBlobService azureBlobService)
+        public LocationCreatedReceiver(IConfiguration configuration, ILogger<ServiceBusReceiver> logger,
+            UserLocationsService userLocationsService, AzureBlobService azureBlobService) : base(configuration, logger)
         {
             this.userLocationsService = userLocationsService;
-            this.logger = logger;
             this.azureBlobService = azureBlobService;
         }
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+
+        protected override async Task ProcessEventAsync(LocationsCreatedMessage message, CancellationToken cancellationToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            var userId = message.UserId;
+            using (var stream = await azureBlobService.DownloadFile(userId))
             {
-                await Task.Delay(10000);
-                try
+                if (stream != null)
                 {
-
-                    using (var stream = await azureBlobService.DownloadFile("333"))
+                    stream.Position = 0;
+                    var folderPath = Path.Combine(Directory.GetCurrentDirectory(), userId);
+                    Directory.CreateDirectory(Path.Combine(folderPath));
+                    var uploadedFilePath = Path.Combine(folderPath, Path.GetRandomFileName());
+                    await using (Stream fileStream = new FileStream(uploadedFilePath, FileMode.Create))
                     {
-                        if (stream != null)
-                        {
-                            stream.Position = 0;
-                            var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "333");
-                            Directory.CreateDirectory(Path.Combine(folderPath));
-                            var uploadedFilePath = Path.Combine(folderPath, Path.GetRandomFileName());
-                            await using (Stream fileStream = new FileStream(uploadedFilePath, FileMode.Create))
-                            {
-                                await stream.CopyToAsync(fileStream);
-                            }
-
-                            var extractedDirectoryPath = Directory.CreateDirectory(Path.Combine(folderPath, "data"));
-                            ZipFile.ExtractToDirectory(uploadedFilePath, extractedDirectoryPath.FullName);
-
-                            var jsonData = GetJsonFilePath(extractedDirectoryPath.FullName);
-                            var userId = new DirectoryInfo(uploadedFilePath).Name;
-                            await userLocationsService.CreateUserLocationsAsync(userId, jsonData);
-
-                            Directory.Delete(folderPath, true);
-                        }
+                        await stream.CopyToAsync(fileStream);
                     }
 
-                    
+                    var extractedDirectoryPath = Directory.CreateDirectory(Path.Combine(folderPath, "data"));
+                    ZipFile.ExtractToDirectory(uploadedFilePath, extractedDirectoryPath.FullName);
 
-                    //var folders = Directory.GetDirectories(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"));
-                    //foreach (var folder in folders)
-                    //{
-                    //    var file = Directory.GetFiles(folder).FirstOrDefault();
-                    //    var extractedDirectoryPath = Directory.CreateDirectory(Path.Combine(folder, "data"));
-                    //    ZipFile.ExtractToDirectory(file, extractedDirectoryPath.FullName);
+                    var jsonData = GetJsonFilePath(extractedDirectoryPath.FullName);
+                    await userLocationsService.CreateUserLocationsAsync(userId, jsonData);
 
-                    //    var jsonData = GetJsonFilePath(extractedDirectoryPath.FullName);
-                    //    var userId = new DirectoryInfo(folder).Name;
-                    //    await userLocationsService.CreateUserLocationsAsync(userId, jsonData);
-
-                    //    Directory.Delete(folder, true);
-                    //}
-
+                    Directory.Delete(folderPath, true);
                 }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, nameof(FileParseBackgroundService));
-                }
-
-                await Task.Delay(10000);
             }
         }
 
@@ -129,6 +153,24 @@ namespace API.Services
 
             jsonFilePath = default;
             return false;
+        }
+    }
+    public class FileParseBackgroundService : BackgroundService
+    {
+        private readonly IServiceScopeFactory serviceScopeFactory;
+
+        public FileParseBackgroundService(IServiceScopeFactory serviceScopeFactory)
+        {
+
+            this.serviceScopeFactory = serviceScopeFactory;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var service = scope.ServiceProvider.GetRequiredService<LocationCreatedReceiver>();
+                service.RegisterMessageHandler();
+            }
         }
     }
 }
